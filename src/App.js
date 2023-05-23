cb8e20b72044651272add9c67d6ae1ed37b0d0da
Sean Siganoff <nationwideclassics@yahoo.com>
2023-05-23 16:35:09 -0400

    added my name first

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Random Name Generator</h1>
+        <h4>Sean Siganoff</h4>
       </header>
     </div>
   );
